fd10afc0187e4b4b72183834de3f5a16eac89311
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-14 23:17:30 -0700

    Finished adding questions, and added in spinner function (why not, hey?)

--- survey.js
+++ survey.js
@@ -5,27 +5,45 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  rl.question('What do you think of Node.js? ', (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-    rl.question('What do you think of Node.js? ', (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-      rl.question('What do you think of Node.js? ', (answer) => {
-        console.log(`Thank you for your valuable feedback: ${answer}`);
+const animation = "|/-\\|/-\\|";
+let timer = 100;
 
-        rl.close();
+const spinner = () => {
+  for (const char of animation) {
+    setTimeout(() => {
+      process.stdout.write(`\r${char}   Generating Profile...`);
+    }, timer);
+    timer += 200;
+  }
+  console.log('hello');
+};
+
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer1) => {
+  console.log(`Thank you for your valuable feedback: ${answer1}`);
+  rl.question('What\'s an activity you like doing? ', (answer2) => {
+    console.log(`Thank you for your valuable feedback: ${answer2}`);
+    rl.question('What do you listen to while doing that? ', (answer3) => {
+      console.log(`Thank you for your valuable feedback: ${answer3}`);
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer4) => {
+        console.log(`Thank you for your valuable feedback: ${answer4}`);
+        rl.question('What\'s your favourite thing to eat for that meal? ', (answer5) => {
+          console.log(`Thank you for your valuable feedback: ${answer5}`);
+          rl.question('Which sport is your absolute favourite ? ', (answer6) => {
+            console.log(`Thank you for your valuable feedback: ${answer6}`);
+            rl.question('What is your superpower ? In a few words, tell us what you are amazing at! ', (answer7) => {
+              console.log(`Thank you for your valuable feedback: ${answer7}`);
+              spinner();
+
+              rl.close();
+            });
+          });
+        });
       });
     });
   });
 });
 
-
-// What's your name? Nicknames are also acceptable :)
-// What's an activity you like doing?
-// What do you listen to while doing that?
-// Which meal is your favourite (eg: dinner, brunch, etc.)
-// What's your favourite thing to eat for that meal?
-// Which sport is your absolute favourite?
-// What is your superpower? In a few words, tell us what you are amazing at!
+// const profile = function() {
+//   console.log(`${answer1} loves listening to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}. `);
+// }
 
