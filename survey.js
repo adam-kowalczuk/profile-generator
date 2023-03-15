b38f357745daec39a9f57da12278d971c798dbdf
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-14 23:44:49 -0700

    Got callbacks working and spinner animation looks good, but I don't like the overall structure of this thing. Gonna need a hand to refactor.

--- survey.js
+++ survey.js
@@ -8,32 +8,42 @@ const rl = readline.createInterface({
 const animation = "|/-\\|/-\\|";
 let timer = 100;
 
-const spinner = () => {
+const spinner = (callback) => {
   for (const char of animation) {
     setTimeout(() => {
       process.stdout.write(`\r${char}   Generating Profile...`);
     }, timer);
     timer += 200;
   }
-  console.log('hello');
+  setTimeout(() => {
+    callback();
+  }, 1700);
+
 };
 
 rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer1) => {
   console.log(`Thank you for your valuable feedback: ${answer1}`);
+  answersArr.push(answer1);
   rl.question('What\'s an activity you like doing? ', (answer2) => {
     console.log(`Thank you for your valuable feedback: ${answer2}`);
+    answersArr.push(answer2);
     rl.question('What do you listen to while doing that? ', (answer3) => {
       console.log(`Thank you for your valuable feedback: ${answer3}`);
+      answersArr.push(answer3);
       rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer4) => {
         console.log(`Thank you for your valuable feedback: ${answer4}`);
+        answersArr.push(answer4);
         rl.question('What\'s your favourite thing to eat for that meal? ', (answer5) => {
           console.log(`Thank you for your valuable feedback: ${answer5}`);
+          answersArr.push(answer5);
           rl.question('Which sport is your absolute favourite ? ', (answer6) => {
             console.log(`Thank you for your valuable feedback: ${answer6}`);
+            answersArr.push(answer6);
             rl.question('What is your superpower ? In a few words, tell us what you are amazing at! ', (answer7) => {
               console.log(`Thank you for your valuable feedback: ${answer7}`);
-              spinner();
-
+              answersArr.push(answer7);
+              spinner(profile);
+              // profile();
               rl.close();
             });
           });
@@ -43,7 +53,10 @@ rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer1) =>
   });
 });
 
-// const profile = function() {
-//   console.log(`${answer1} loves listening to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}. `);
-// }
+answersArr = [];
+
+const profile = function() {
+  console.log(`\n${answersArr[0]} loves listening to ${answersArr[2]} while ${answersArr[1]}, devouring ${answersArr[4]} for ${answersArr[3]}, prefers ${answersArr[5]} over any other sport, and is amazing at ${answersArr[6]}. `);
+}
+
 
