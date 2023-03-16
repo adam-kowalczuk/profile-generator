2c4a63a50a2d2b80aef57e91f934f7b2db9e775a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-16 11:22:42 -0700

    Removed messages from being printed after every answer input

--- survey.js
+++ survey.js
@@ -28,30 +28,22 @@ const spinner = (action) => {
 };
 
 rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
   answersArr.push(answer);
   rl.question('What\'s an activity you like doing? ', (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
     answersArr.push(answer);
     rl.question('What do you listen to while doing that? ', (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`);
       answersArr.push(answer);
       rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
-        console.log(`Thank you for your valuable feedback: ${answer}`);
         answersArr.push(answer);
         rl.question('What\'s your favourite thing to eat for that meal? ', (answer) => {
-          console.log(`Thank you for your valuable feedback: ${answer}`);
           answersArr.push(answer);
           rl.question('Which sport is your absolute favourite ? ', (answer) => {
-            console.log(`Thank you for your valuable feedback: ${answer}`);
             answersArr.push(answer);
             rl.question('What is your superpower ? In a few words, tell us what you are amazing at! ', (answer) => {
-              console.log(`Thank you for your valuable feedback: ${answer}`);
               answersArr.push(answer);
+              rl.close();
               spinner('Generating Profile...');
               generatedProfile();
-
-              rl.close();
             });
           });
         });
